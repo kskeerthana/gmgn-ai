f4c984f90692e4dc453e3b0879f2b3cd131cd265
Keerthana Srinivasan <57140272+kskeerthana@users.noreply.github.com>
2025-06-13 21:27:32 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,12 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  base: '/gmgn-ai-react-clone-main/', // Replace with your repo name
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
